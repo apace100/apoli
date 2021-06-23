0137cf4af6e9edd698c4bc4ab5597487d948a8b2
Apace <apace100@web.de>
2021-06-23 22:02:17 +0200

    Fixed hud renders, oops

--- src/main/java/io/github/apace100/apoli/screen/PowerHudRenderer.java
+++ src/main/java/io/github/apace100/apoli/screen/PowerHudRenderer.java
@@ -54,9 +54,9 @@ public class PowerHudRenderer extends DrawableHelper implements GameHudRender {
                 drawTexture(matrices, x, y, 0, 0, barWidth, 5);
                 int v = 8 + render.getBarIndex() * 10;
                 int w = (int)(hudPower.getFill() * barWidth);
-                drawTexture(matrices, x, y, 0, v, w, barHeight);
+                drawTexture(matrices, x, y - 2, 0, v, w, barHeight);
                 setZOffset(getZOffset() + 1);
-                drawTexture(matrices, x - iconSize - 2, y - 2, 73, v - 2, iconSize, iconSize);
+                drawTexture(matrices, x - iconSize - 2, y - 2, 73, v, iconSize, iconSize);
                 setZOffset(getZOffset() - 1);
                 y -= 8;
             }
