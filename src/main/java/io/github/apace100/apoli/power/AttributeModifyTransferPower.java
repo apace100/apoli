8e5317acbf27e3c623ae124904fd5d839c1a9e66
Apace <apace100@web.de>
2021-10-01 14:31:16 +0200

    Removed debug log output

--- src/main/java/io/github/apace100/apoli/power/AttributeModifyTransferPower.java
+++ src/main/java/io/github/apace100/apoli/power/AttributeModifyTransferPower.java
@@ -1,6 +1,5 @@
 package io.github.apace100.apoli.power;
 
-import io.github.apace100.apoli.Apoli;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.attribute.EntityAttribute;
@@ -33,7 +32,6 @@ public class AttributeModifyTransferPower extends Power {
             attributeInstance.getModifiers().forEach(mod -> {
                 EntityAttributeModifier transferMod =
                     new EntityAttributeModifier(mod.getName(), mod.getValue() * valueMultiplier, mod.getOperation());
-                Apoli.LOGGER.info("  Transferring attribute: " + mod.getName());
                 modifiers.add(transferMod);
             });
         }
