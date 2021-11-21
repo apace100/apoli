987153556030afcf5f9b01426cc9203d7ec9e6a8
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-11-21 12:44:01 +1100

    Added Bi-entity conditions and actions to ModifyDamageDealtPower and ModifyDamageTakenPower

--- src/main/java/io/github/apace100/apoli/power/ModifyDamageTakenPower.java
+++ src/main/java/io/github/apace100/apoli/power/ModifyDamageTakenPower.java
@@ -20,17 +20,20 @@ import java.util.function.Predicate;
 public class ModifyDamageTakenPower extends ValueModifyingPower {
 
     private final Predicate<Pair<DamageSource, Float>> condition;
+    private final Predicate<Pair<Entity, Entity>> biEntityCondition;
 
     private Consumer<Entity> attackerAction;
     private Consumer<Entity> selfAction;
+    private Consumer<Pair<Entity, Entity>> biEntityAction;
 
-    public ModifyDamageTakenPower(PowerType<?> type, LivingEntity entity, Predicate<Pair<DamageSource, Float>> condition) {
+    public ModifyDamageTakenPower(PowerType<?> type, LivingEntity entity, Predicate<Pair<DamageSource, Float>> condition, Predicate<Pair<Entity, Entity>> biEntityCondition) {
         super(type, entity);
         this.condition = condition;
+        this.biEntityCondition = biEntityCondition;
     }
 
     public boolean doesApply(DamageSource source, float damageAmount) {
-        return condition.test(new Pair(source, damageAmount));
+        return source.getAttacker() == null ? this.condition.test(new Pair(source, damageAmount)) && biEntityCondition == null : this.condition.test(new Pair(source, damageAmount)) && (biEntityCondition == null || biEntityCondition.test(new Pair(entity, source.getAttacker())));
     }
 
     public void setAttackerAction(Consumer<Entity> attackerAction) {
@@ -41,6 +44,10 @@ public class ModifyDamageTakenPower extends ValueModifyingPower {
         this.selfAction = selfAction;
     }
 
+    public void setBiEntityAction(Consumer<Pair<Entity, Entity>> biEntityAction) {
+        this.biEntityAction = biEntityAction;
+    }
+
     public void executeActions(Entity attacker) {
         if(selfAction != null) {
             selfAction.accept(entity);
@@ -48,25 +55,34 @@ public class ModifyDamageTakenPower extends ValueModifyingPower {
         if(attackerAction != null) {
             attackerAction.accept(attacker);
         }
+        if(biEntityAction != null) {
+            biEntityAction.accept(new Pair<>(entity, attacker));
+        }
     }
 
     public static PowerFactory createFactory() {
         return new PowerFactory<>(Apoli.identifier("modify_damage_taken"),
             new SerializableData()
                 .add("damage_condition", ApoliDataTypes.DAMAGE_CONDITION, null)
+                .add("bientity_condition", ApoliDataTypes.BIENTITY_CONDITION, null)
                 .add("modifier", SerializableDataTypes.ATTRIBUTE_MODIFIER, null)
                 .add("modifiers", SerializableDataTypes.ATTRIBUTE_MODIFIERS, null)
                 .add("self_action", ApoliDataTypes.ENTITY_ACTION, null)
-                .add("attacker_action", ApoliDataTypes.ENTITY_ACTION, null),
+                .add("attacker_action", ApoliDataTypes.ENTITY_ACTION, null)
+                .add("bientity_action", ApoliDataTypes.BIENTITY_ACTION, null),
             data ->
                 (type, player) -> {
                     ModifyDamageTakenPower power = new ModifyDamageTakenPower(type, player,
-                        data.isPresent("damage_condition") ? (ConditionFactory<Pair<DamageSource, Float>>.Instance)data.get("damage_condition") : dmg -> true);
+                        data.isPresent("damage_condition") ? (ConditionFactory<Pair<DamageSource, Float>>.Instance)data.get("damage_condition") : dmg -> true,
+                        (ConditionFactory<Pair<Entity, Entity>>.Instance)data.get("bientity_condition"));
                     if(data.isPresent("modifier")) {
                         power.addModifier(data.getModifier("modifier"));
                     }
                     if(data.isPresent("modifiers")) {
                         ((List<EntityAttributeModifier>)data.get("modifiers")).forEach(power::addModifier);
+                    }
+                    if(data.isPresent("bientity_action")) {
+                        power.setBiEntityAction((ActionFactory<Pair<Entity, Entity>>.Instance)data.get("bientity_action"));
                     }
                     if(data.isPresent("self_action")) {
                         power.setSelfAction((ActionFactory<Entity>.Instance)data.get("self_action"));
