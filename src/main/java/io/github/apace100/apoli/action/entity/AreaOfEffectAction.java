7d0f7e2309c86ae6994d6ef1990f2815dce85f61
Apace <apace100@web.de>
2021-11-30 15:15:03 +0100

    Modified `area_of_effect` fields
    + Added `bientity_action` field
    - Removed `action` and `actions` field
    - Removed `condition` field
    * Renamed `bi_condition` to `bientity_condition` for consistency

--- src/main/java/io/github/apace100/apoli/action/entity/AreaOfEffectAction.java
+++ src/main/java/io/github/apace100/apoli/action/entity/AreaOfEffectAction.java
@@ -9,48 +9,34 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.Pair;
 import net.minecraft.util.math.Box;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class AreaOfEffectAction {
     public static void action(SerializableData.Instance data, Entity entity) {
-        List<Consumer<Entity>> actions = new ArrayList<>();
-
-        if (data.isPresent("action")) {
-            actions.add((Consumer<Entity>) data.get("action"));
-        }
-
-        if (data.isPresent("actions")) {
-            actions.addAll((Collection<? extends Consumer<Entity>>) data.get("actions"));
-        }
-
-        Predicate<Entity> predicate = data.isPresent("condition") ? (Predicate<Entity>) data.get("condition") : x -> true;
-        Predicate<Pair<Entity, Entity>> bi_predicate = data.isPresent("bi_condition") ? (Predicate<Pair<Entity, Entity>>) data.get("condition") : x -> true;
-        boolean includeTarget = (boolean) data.get("include_target");
-        double radius = (double) data.get("radius");
+        Consumer<Pair<Entity, Entity>> bientityAction = data.get("bientity_action");
+        Predicate<Pair<Entity, Entity>> bientityCondition = data.get("bientity_condition");
+        boolean includeTarget = data.get("include_target");
+        double radius = data.get("radius");
         double diameter = radius * 2;
 
         for (Entity check : entity.world.getNonSpectatingEntities(Entity.class, Box.of(entity.getLerpedPos(1F), diameter, diameter, diameter))) {
             if (check == entity && !includeTarget)
                 continue;
-            if (predicate.test(check) && bi_predicate.test(new Pair<>(entity, check)) && check.squaredDistanceTo(entity) < radius * radius)
-                actions.forEach(x -> x.accept(check));
+            Pair<Entity, Entity> actorTargetPair = new Pair<>(entity, check);
+            if ((bientityCondition == null || bientityCondition.test(actorTargetPair)) && check.squaredDistanceTo(entity) < radius * radius)
+                bientityAction.accept(actorTargetPair);
         }
     }
 
     public static ActionFactory<Entity> createFactory() {
-        return new ActionFactory<Entity>(Apoli.identifier("area_of_effect"),
-                new SerializableData()
-                        .add("radius", SerializableDataTypes.DOUBLE, 16D)
-                        .add("action", ApoliDataTypes.ENTITY_ACTION, null)
-                        .add("actions", ApoliDataTypes.ENTITY_ACTIONS, null)
-                        .add("condition", ApoliDataTypes.ENTITY_CONDITION, null)
-                        .add("bi_condition", ApoliDataTypes.BIENTITY_CONDITION, null)
-                        .add("include_target", SerializableDataTypes.BOOLEAN, false),
-                AreaOfEffectAction::action
+        return new ActionFactory<>(Apoli.identifier("area_of_effect"),
+            new SerializableData()
+                .add("radius", SerializableDataTypes.DOUBLE, 16D)
+                .add("bientity_action", ApoliDataTypes.BIENTITY_ACTION)
+                .add("bientity_condition", ApoliDataTypes.BIENTITY_CONDITION, null)
+                .add("include_target", SerializableDataTypes.BOOLEAN, false),
+            AreaOfEffectAction::action
         );
     }
 }
