5fa23c36a823e6cc41a71c15e64fc4dbb664ee5b
Apace <apace100@web.de>
2021-12-04 20:44:59 +0100

    Fixed `crafting_table` action not producing output.

--- src/main/java/io/github/apace100/apoli/mixin/CraftingScreenHandlerMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/CraftingScreenHandlerMixin.java
@@ -6,17 +6,30 @@ import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.CraftingResultInventory;
 import net.minecraft.screen.CraftingScreenHandler;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CraftingScreenHandler.class)
 public class CraftingScreenHandlerMixin {
 
+    @Shadow @Final private ScreenHandlerContext context;
+
     @Inject(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/RecipeManager;getFirstMatch(Lnet/minecraft/recipe/RecipeType;Lnet/minecraft/inventory/Inventory;Lnet/minecraft/world/World;)Ljava/util/Optional;"))
     private static void clearPowerCraftingInventory(ScreenHandler handler, World world, PlayerEntity player, CraftingInventory craftingInventory, CraftingResultInventory resultInventory, CallbackInfo ci) {
         ((PowerCraftingInventory)craftingInventory).setPower(null);
+    }
+
+    @Inject(method = "canUse", at = @At("HEAD"), cancellable = true)
+    private void allowUsingViaPower(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+        if(context.get((world, pos) -> pos.equals(player.getBlockPos()), false)) {
+            cir.setReturnValue(true);
+        }
     }
 }
