3f1c81d113f560ef6a485a809c32ee30279adea5
Apace <apace100@web.de>
2021-12-10 16:42:14 +0100

    Fixed crash caused by status effect modifying powers

--- src/main/java/io/github/apace100/apoli/mixin/StatusEffectInstanceMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/StatusEffectInstanceMixin.java
@@ -1,12 +1,13 @@
 package io.github.apace100.apoli.mixin;
 
+import io.github.apace100.apoli.access.HiddenEffectStatus;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(StatusEffectInstance.class)
-public class StatusEffectInstanceMixin {
+public class StatusEffectInstanceMixin implements HiddenEffectStatus {
     @Shadow
     @Nullable
     private StatusEffectInstance hiddenEffect;
