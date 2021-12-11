434a118654e28888a1611324d0deba0f77656293
Apace <apace100@web.de>
2021-12-12 00:32:44 +0100

    Added usability tooltips and bumped version to 2.2.2

--- src/main/java/io/github/apace100/apoli/mixin/ItemStackMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/ItemStackMixin.java
@@ -1,8 +1,10 @@
 package io.github.apace100.apoli.mixin;
 
+import io.github.apace100.apoli.Apoli;
 import io.github.apace100.apoli.access.MutableItemStack;
 import io.github.apace100.apoli.component.PowerHolderComponent;
 import io.github.apace100.apoli.power.*;
+import io.github.apace100.apoli.util.ApoliConfigClient;
 import io.github.apace100.apoli.util.StackPowerUtil;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EquipmentSlot;
@@ -12,12 +14,15 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -27,15 +32,59 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
+import java.util.Locale;
 
 @Mixin(ItemStack.class)
-public class ItemStackMixin implements MutableItemStack {
+public abstract class ItemStackMixin implements MutableItemStack {
 
     @Shadow @Deprecated private Item item;
 
     @Shadow private NbtCompound nbt;
 
     @Shadow private int count;
+
+    @Shadow public abstract UseAction getUseAction();
+
+    @Inject(method = "getTooltip", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 0, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void addUnusableTooltip(@Nullable PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir, List<Text> list) {
+        if(player != null) {
+            ApoliConfigClient.Tooltips config = ((ApoliConfigClient) Apoli.config).tooltips;
+            if(!config.showUsabilityHints) {
+                return;
+            }
+            List<PreventItemUsePower> powers = PowerHolderComponent.getPowers(player, PreventItemUsePower.class).stream().filter(p -> p.doesPrevent((ItemStack)(Object)this)).toList();
+            int powerCountWithHidden = powers.size();
+            powers = powers.stream().filter(p -> !p.getType().isHidden()).toList();
+            if(powerCountWithHidden == 0) {
+                return;
+            }
+            String translationKeyBase = "tooltip.apoli.unusable." + getUseAction().name().toLowerCase(Locale.ROOT);
+            Formatting textColor = Formatting.GRAY;
+            Formatting powerColor = Formatting.RED;
+            if(config.compactUsabilityHints || powers.size() == 0) {
+                if(powers.size() == 1) {
+                    PreventItemUsePower power = powers.get(0);
+                    MutableText preventText = new TranslatableText(translationKeyBase + ".single",
+                        power.getType().getName().formatted(powerColor)).formatted(textColor);
+                    list.add(preventText);
+                } else {
+                    list.add(
+                        new TranslatableText(translationKeyBase + ".multiple",
+                            new LiteralText((powers.size() == 0 ? powerCountWithHidden : powers.size()) + "").formatted(powerColor))
+                            .formatted(textColor));
+                }
+            } else {
+                MutableText powerNameList = powers.get(0).getType().getName().formatted(powerColor);
+                for(int i = 1; i < powers.size(); i++) {
+                    powerNameList = powerNameList.append(new LiteralText(", ").formatted(textColor));
+                    powerNameList = powerNameList.append(powers.get(i).getType().getName().formatted(powerColor));
+                }
+                MutableText preventText = new TranslatableText(translationKeyBase + ".single",
+                    powerNameList).formatted(textColor);
+                list.add(preventText);
+            }
+        }
+    }
 
     @Inject(method = "getTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;hasNbt()Z", ordinal = 1), locals = LocalCapture.CAPTURE_FAILHARD)
     private void addEquipmentPowerTooltips(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir, List<Text> list) {
