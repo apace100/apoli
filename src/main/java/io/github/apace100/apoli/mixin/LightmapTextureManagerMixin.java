7b47532b057831553843054145623cbb85058a0d
Apace <apace100@web.de>
2022-06-07 02:59:11 +0200

    Fixed night vision power type

--- src/main/java/io/github/apace100/apoli/mixin/LightmapTextureManagerMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/LightmapTextureManagerMixin.java
@@ -20,7 +20,7 @@ public abstract class LightmapTextureManagerMixin implements AutoCloseable {
 
     @Shadow @Final private MinecraftClient client;
 
-    @ModifyVariable(method = "update", at = @At(value = "STORE"), ordinal = 3)
+    @ModifyVariable(method = "update", at = @At(value = "STORE"), ordinal = 6)
     private float nightVisionPowerEffect(float value) {
         Optional<Float> nightVisionStrength = PowerHolderComponent.KEY.get(client.player).getPowers(NightVisionPower.class).stream().filter(NightVisionPower::isActive).map(NightVisionPower::getStrength).max(Float::compareTo);
         return nightVisionStrength.map(aFloat -> Math.max(aFloat, value)).orElse(value);
