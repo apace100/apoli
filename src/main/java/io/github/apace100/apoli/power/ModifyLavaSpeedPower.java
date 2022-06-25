c2b12ed51b8da53287c1047b2bc2c17906b2dea2
Apace <apace100@web.de>
2022-06-25 10:21:19 +0200

    Made conditions apply to `modify_swim_speed` and `modify_lava_speed`

--- src/main/java/io/github/apace100/apoli/power/ModifyLavaSpeedPower.java
+++ src/main/java/io/github/apace100/apoli/power/ModifyLavaSpeedPower.java
@@ -3,6 +3,7 @@ package io.github.apace100.apoli.power;
 import de.dafuqs.additionalentityattributes.AdditionalEntityAttributes;
 import io.github.apace100.apoli.Apoli;
 import io.github.apace100.apoli.power.factory.PowerFactory;
+import io.github.apace100.apoli.util.AttributedEntityAttributeModifier;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.LivingEntity;
@@ -10,9 +11,9 @@ import net.minecraft.entity.attribute.EntityAttributeModifier;
 
 import java.util.List;
 
-public class ModifyLavaSpeedPower extends AttributePower {
+public class ModifyLavaSpeedPower extends ConditionedAttributePower {
     public ModifyLavaSpeedPower(PowerType<?> type, LivingEntity entity) {
-        super(type, entity, false);
+        super(type, entity, 10,false);
     }
 
     public static PowerFactory createFactory() {
@@ -24,9 +25,11 @@ public class ModifyLavaSpeedPower extends AttributePower {
                 (type, player) -> {
                     ModifyLavaSpeedPower power = new ModifyLavaSpeedPower(type, player);
                     data.<EntityAttributeModifier>ifPresent
-                        ("modifier", mod -> power.addModifier(AdditionalEntityAttributes.LAVA_SPEED, mod));
+                        ("modifier", mod -> power.addModifier(
+                            new AttributedEntityAttributeModifier(AdditionalEntityAttributes.LAVA_SPEED, mod)));
                     data.<List<EntityAttributeModifier>>ifPresent("modifiers",
-                        mods -> mods.forEach(mod -> power.addModifier(AdditionalEntityAttributes.LAVA_SPEED, mod))
+                        mods -> mods.forEach(mod -> power.addModifier(
+                            new AttributedEntityAttributeModifier(AdditionalEntityAttributes.LAVA_SPEED, mod)))
                     );
                     return power;
                 })
