8d7f55791e199a75811b3a7b8b8728144d61b8b5
Robin <36313416+ThatRobin@users.noreply.github.com>
2023-01-31 01:55:28 +0000

    Added Factory.java, an interface that allows you to call getSerializerId without the requirement of specifying what type of factory you are handling.

--- src/main/java/io/github/apace100/apoli/power/factory/Factory.java
+++ src/main/java/io/github/apace100/apoli/power/factory/Factory.java
@@ -1,7 +1,5 @@
 package io.github.apace100.apoli.power.factory;
 
-import com.google.gson.JsonObject;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 
 public interface Factory {
