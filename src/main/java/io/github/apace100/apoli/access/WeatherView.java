a4797e9f403fd1852e75f3de6e44cd5fc701606b
eggohito <66972816+eggohito@users.noreply.github.com>
2023-07-18 00:30:56 +0800

    Simplified impl. for `in_snow` and `in_thunderstorm` entity conditions

--- src/main/java/io/github/apace100/apoli/access/WeatherView.java
+++
@@ -1,8 +0,0 @@
-package io.github.apace100.apoli.access;
-
-import net.minecraft.util.math.BlockPos;
-
-public interface WeatherView {
-	boolean inSnow(BlockPos... pos);
-	boolean inThunderStorm(BlockPos... pos);
-}
--- src/main/java/io/github/apace100/apoli/power/factory/condition/entity/InSnowCondition.java
+++ src/main/java/io/github/apace100/apoli/power/factory/condition/entity/InSnowCondition.java
@@ -1,8 +1,8 @@
 package io.github.apace100.apoli.power.factory.condition.entity;
 
 import io.github.apace100.apoli.Apoli;
-import io.github.apace100.apoli.access.WeatherView;
 import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
+import io.github.apace100.apoli.util.WorldUtil;
 import io.github.apace100.calio.data.SerializableData;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
@@ -14,7 +14,7 @@ public class InSnowCondition {
 		BlockPos downBlockPos = entity.getBlockPos();
 		BlockPos upBlockPos = BlockPos.ofFloored(downBlockPos.getX(), entity.getBoundingBox().maxY, downBlockPos.getX());
 
-		return ((WeatherView) entity.getWorld()).inSnow(downBlockPos, upBlockPos);
+		return WorldUtil.inSnow(entity.getWorld(), downBlockPos, upBlockPos);
 
 	}
 
