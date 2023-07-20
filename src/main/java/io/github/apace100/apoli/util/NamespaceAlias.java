68d17bccda032b8d0e51b8af831a89746d883244
eggohito <66972816+eggohito@users.noreply.github.com>
2023-07-21 02:40:28 +0800

    Added identifier aliasing and did some tidying

--- src/main/java/io/github/apace100/apoli/util/NamespaceAlias.java
+++ src/main/java/io/github/apace100/apoli/util/NamespaceAlias.java
@@ -1,13 +1,12 @@
 package io.github.apace100.apoli.util;
 
-import io.github.apace100.apoli.Apoli;
 import net.minecraft.util.Identifier;
 
-import java.util.HashMap;
-
-public final class NamespaceAlias {
-
-    private static final HashMap<String, String> aliasedNamespaces = new HashMap<>();
+/**
+ *  A utility class for adding aliases to namespaces. This is <b>deprecated</b>, use {@link IdentifierAlias} instead.
+ */
+@Deprecated(forRemoval = true)
+public final class NamespaceAlias extends AliasSupplier {
 
     public static void addAlias(String fromNamespace, String toNamespace) {
         aliasedNamespaces.put(fromNamespace, toNamespace);
@@ -23,7 +22,7 @@ public final class NamespaceAlias {
 
     public static Identifier resolveAlias(Identifier original) {
         if(!aliasedNamespaces.containsKey(original.getNamespace())) {
-            throw new RuntimeException("Tried to resolve a namespace alias for a namespace which didn't have an alias.");
+            throw NO_NAMESPACE_ALIAS;
         }
         return new Identifier(aliasedNamespaces.get(original.getNamespace()), original.getPath());
     }
