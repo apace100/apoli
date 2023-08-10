7cbb7ee3a5a5afcbba455d5ea2d2249b62031d1c
eggohito <66972816+eggohito@users.noreply.github.com>
2023-08-11 05:54:42 +0800

    Made the `tooltip` power type resolve JSON text components server-side (#120)
    * Tweaked the `tooltip` power type
    + It can now resolve JSON text components server-side
    + Added `tick_rate` field with a default value of 20
    
    * Tweaked impl. of the `tooltip` power type again
    * Renamed a variable and re-worded the warning message for parsing the tooltip text(s)
    
    * Added `should_resolve` boolean field to `tooltip` power type
    Which determines whether the specified text components should be resolved
    
    * Fix oopsie in the `tooltip` power type
    Now uses the other list of texts to be added to the tooltip if the power is set to not resolve those texts
    
    * Fixed another oopsie in `tooltip` power type
    `should_resolve` is now saved to NBT
    
    * Added testdata for `tooltip` power type

--- src/main/java/io/github/apace100/apoli/power/TooltipPower.java
+++ src/main/java/io/github/apace100/apoli/power/TooltipPower.java
@@ -1,15 +1,26 @@
 package io.github.apace100.apoli.power;
 
+import com.google.common.collect.Lists;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.github.apace100.apoli.Apoli;
+import io.github.apace100.apoli.component.PowerHolderComponent;
 import io.github.apace100.apoli.data.ApoliDataTypes;
 import io.github.apace100.apoli.power.factory.PowerFactory;
 import io.github.apace100.calio.data.SerializableData;
-import io.github.apace100.calio.data.SerializableDataType;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtString;
+import net.minecraft.server.command.CommandOutput;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import net.minecraft.text.Texts;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -17,47 +28,162 @@ import java.util.function.Predicate;
 public class TooltipPower extends Power {
 
     private final Predicate<ItemStack> itemCondition;
-    private final List<Text> texts = new LinkedList<>();
+    private final List<Text> texts;
+    private final int tickRate;
     private final int order;
 
-    public TooltipPower(PowerType<?> type, LivingEntity entity, Predicate<ItemStack> itemCondition, int order) {
+    private List<Text> tooltipTexts;
+    private Integer initialTicks;
+    private boolean shouldResolve;
+
+    public TooltipPower(PowerType<?> type, LivingEntity entity, Predicate<ItemStack> itemCondition, Text text, List<Text> texts, boolean shouldResolve, int tickRate, int order) {
         super(type, entity);
+        this.texts = new LinkedList<>();
+        this.tooltipTexts = new LinkedList<>();
+        if (text != null) {
+            this.texts.add(text);
+        }
+        if (texts != null) {
+            this.texts.addAll(texts);
+        }
         this.itemCondition = itemCondition;
+        this.shouldResolve = shouldResolve;
+        this.tickRate = tickRate <= 0 ? 1 : tickRate;
         this.order = order;
+        if (shouldResolve) {
+            this.setTicking(true);
+        }
+    }
+
+    @Override
+    public void tick() {
+
+        if (isActive()) {
+
+            if (initialTicks == null) {
+                initialTicks = entity.age % tickRate;
+                return;
+            }
+
+            if (entity.age % tickRate != initialTicks) {
+                return;
+            }
+
+            List<Text> parsedTexts = parseTexts();
+            if (parsedTexts.isEmpty() || !Collections.disjoint(tooltipTexts, parsedTexts)) {
+                return;
+            }
+
+            tooltipTexts = parsedTexts;
+            PowerHolderComponent.syncPower(entity, this.getType());
+
+        } else if (initialTicks != null) {
+            initialTicks = null;
+        }
+
+    }
+
+    @Override
+    public NbtElement toTag() {
+
+        NbtCompound rootNbt = new NbtCompound();
+        NbtList tooltipTextsNbt = new NbtList();
+
+        for (Text tooltipText : tooltipTexts) {
+            NbtString tooltipTextNbt = NbtString.of(Text.Serializer.toJson(tooltipText));
+            tooltipTextsNbt.add(tooltipTextNbt);
+        }
+
+        rootNbt.put("Tooltips", tooltipTextsNbt);
+        rootNbt.putBoolean("ShouldResolve", shouldResolve);
+        return rootNbt;
+
+    }
+
+    @Override
+    public void fromTag(NbtElement tag) {
+
+        tooltipTexts.clear();
+        NbtCompound rootNbt = (NbtCompound) tag;
+        NbtList tooltipTextsNbt = rootNbt.getList("Tooltips", NbtElement.STRING_TYPE);
+
+        for (int i = 0; i < tooltipTextsNbt.size(); i++) {
+            Text tooltipText = Text.Serializer.fromJson(tooltipTextsNbt.getString(i));
+            tooltipTexts.add(tooltipText);
+        }
+
+        shouldResolve = rootNbt.getBoolean("ShouldResolve");
+
     }
 
     public int getOrder() {
         return order;
     }
 
-    public void addText(Text text) {
-        texts.add(text);
-    }
-
     public void addToTooltip(List<Text> tooltip) {
-        tooltip.addAll(texts);
+        tooltip.addAll(shouldResolve ? tooltipTexts : texts);
     }
 
     public boolean doesApply(ItemStack stack) {
         return itemCondition == null || itemCondition.test(stack);
     }
 
+    private List<Text> parseTexts() {
+
+        List<Text> parsedTexts = Lists.newLinkedList();
+        if (texts.isEmpty() || entity.getWorld().isClient) {
+            return parsedTexts;
+        }
+
+        ServerCommandSource source = new ServerCommandSource(
+            CommandOutput.DUMMY,
+            entity.getPos(),
+            entity.getRotationClient(),
+            (ServerWorld) entity.getWorld(),
+            Apoli.config.executeCommand.permissionLevel,
+            entity.getEntityName(),
+            entity.getName(),
+            entity.getWorld().getServer(),
+            entity
+        );
+
+        for (int i = 0; i < texts.size(); i++) {
+            try {
+
+                Text text = texts.get(i);
+                Text parsedText = Texts.parse(source, text, entity, 0);
+
+                parsedTexts.add(parsedText);
+
+            } catch (CommandSyntaxException e) {
+                Apoli.LOGGER.warn("Power {} could not parse tooltip text at index {}: {}", this.getType().getIdentifier(), i, e.getMessage());
+            }
+        }
+
+        return parsedTexts;
+
+    }
+
     public static PowerFactory createFactory() {
-        return new PowerFactory<>(Apoli.identifier("tooltip"),
+        return new PowerFactory<>(
+            Apoli.identifier("tooltip"),
             new SerializableData()
                 .add("item_condition", ApoliDataTypes.ITEM_CONDITION, null)
                 .add("text", SerializableDataTypes.TEXT, null)
-                .add("texts", SerializableDataType.list(SerializableDataTypes.TEXT), null)
+                .add("texts", SerializableDataTypes.TEXTS, null)
+                .add("should_resolve", SerializableDataTypes.BOOLEAN, false)
+                .add("tick_rate", SerializableDataTypes.INT, 20)
                 .add("order", SerializableDataTypes.INT, 0),
-            data ->
-                (type, player) -> {
-                    TooltipPower ttp = new TooltipPower(type, player,
-                        data.isPresent("item_condition") ? data.get("item_condition") : null,
-                        data.get("order"));
-                    data.ifPresent("text", ttp::addText);
-                    data.<List<Text>>ifPresent("texts", t -> t.forEach(ttp::addText));
-                    return ttp;
-                })
-            .allowCondition();
+            data -> (powerType, livingEntity) -> new TooltipPower(
+                powerType,
+                livingEntity,
+                data.get("item_condition"),
+                data.get("text"),
+                data.get("texts"),
+                data.get("should_resolve"),
+                data.get("tick_rate"),
+                data.get("order")
+            )
+        ).allowCondition();
     }
 }
