cf3f13a70aa6489a2888ef203885097007826489
eggohito <66972816+eggohito@users.noreply.github.com>
2023-08-14 03:07:54 +0800

    Changed the internals of `modify_lava_speed` and `modify_swim_speed` Now extends `ModifyAttributePower` as opposed to `ConditionedAttributePower`

--- src/main/java/io/github/apace100/apoli/power/ModifySwimSpeedPower.java
+++ src/main/java/io/github/apace100/apoli/power/ModifySwimSpeedPower.java
@@ -3,36 +3,37 @@ package io.github.apace100.apoli.power;
 import de.dafuqs.additionalentityattributes.AdditionalEntityAttributes;
 import io.github.apace100.apoli.Apoli;
 import io.github.apace100.apoli.power.factory.PowerFactory;
-import io.github.apace100.apoli.util.AttributedEntityAttributeModifier;
+import io.github.apace100.apoli.util.modifier.Modifier;
 import io.github.apace100.calio.data.SerializableData;
-import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
 
 import java.util.List;
 
-public class ModifySwimSpeedPower extends ConditionedAttributePower {
-    public ModifySwimSpeedPower(PowerType<?> type, LivingEntity entity) {
-        super(type, entity, 10,false);
+public class ModifySwimSpeedPower extends ModifyAttributePower {
+
+    public ModifySwimSpeedPower(PowerType<?> powerType, LivingEntity livingEntity, Modifier modifier, List<Modifier> modifiers) {
+        super(powerType, livingEntity, AdditionalEntityAttributes.WATER_SPEED);
+        if (modifier != null) {
+            this.addModifier(modifier);
+        }
+        if (modifiers != null) {
+            modifiers.forEach(this::addModifier);
+        }
     }
 
     public static PowerFactory createFactory() {
-        return new PowerFactory<>(Apoli.identifier("modify_swim_speed"),
+        return new PowerFactory<>(
+            Apoli.identifier("modify_swim_speed"),
             new SerializableData()
-                .add("modifier", SerializableDataTypes.ATTRIBUTE_MODIFIER, null)
-                .add("modifiers", SerializableDataTypes.ATTRIBUTE_MODIFIERS, null),
-            data ->
-                (type, player) -> {
-                    ModifySwimSpeedPower power = new ModifySwimSpeedPower(type, player);
-                    data.<EntityAttributeModifier>ifPresent
-                        ("modifier", mod -> power.addModifier(
-                            new AttributedEntityAttributeModifier(AdditionalEntityAttributes.WATER_SPEED, mod)));
-                    data.<List<EntityAttributeModifier>>ifPresent("modifiers",
-                        mods -> mods.forEach(mod -> power.addModifier(
-                            new AttributedEntityAttributeModifier(AdditionalEntityAttributes.WATER_SPEED, mod)))
-                    );
-                    return power;
-                })
-            .allowCondition();
+                .add("modifier", Modifier.DATA_TYPE, null)
+                .add("modifiers", Modifier.LIST_TYPE, null),
+            data -> (powerType, livingEntity) -> new ModifySwimSpeedPower(
+                powerType,
+                livingEntity,
+                data.get("modifier"),
+                data.get("modifier")
+            )
+        ).allowCondition();
     }
+
 }
