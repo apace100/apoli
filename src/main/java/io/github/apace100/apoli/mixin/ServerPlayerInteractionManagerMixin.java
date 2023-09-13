9d8471d0e179c282515343bb421c8308fc0f46e3
eggohito <66972816+eggohito@users.noreply.github.com>
2023-09-14 05:25:35 +0800

    Tweaked impls. of power types related to mining/breaking blocks * Tweaked the code style * Fixed issue with `modify_break_speed`: modified harvest level is now taken account for calculating the block breaking delta value * Fixed issue with `action_on_block_use`: actions are no longer executed twice * Fixed issue with `action_on_block_break`: actions are now executed regardless of gamemode + Added `priority` field to `modify_harvest`

--- src/main/java/io/github/apace100/apoli/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,14 +1,16 @@
 package io.github.apace100.apoli.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import io.github.apace100.apoli.component.PowerHolderComponent;
 import io.github.apace100.apoli.power.ActionOnBlockBreakPower;
 import io.github.apace100.apoli.power.ActionOnBlockUsePower;
 import io.github.apace100.apoli.power.ModifyHarvestPower;
 import io.github.apace100.apoli.power.PreventBlockUsePower;
 import io.github.apace100.apoli.util.SavedBlockPosition;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
@@ -18,56 +20,64 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin {
 
     @Shadow
-    public ServerWorld world;
+    protected ServerWorld world;
+
     @Shadow
-    public ServerPlayerEntity player;
-    private SavedBlockPosition savedBlockPosition;
+    @Final
+    protected ServerPlayerEntity player;
 
     @Inject(method = "tryBreakBlock", at = @At("HEAD"))
-    private void cacheBlock(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        this.savedBlockPosition = new SavedBlockPosition(world, pos);
+    private void apoli$cacheMinedBlock(BlockPos pos, CallbackInfoReturnable<Boolean> cir, @Share("cachedMinedBlock") LocalRef<SavedBlockPosition> cachedMinedBlockRef, @Share("modifiedCanHarvest") LocalBooleanRef modifiedCanHarvestRef) {
+        cachedMinedBlockRef.set(new SavedBlockPosition(world, pos));
+        modifiedCanHarvestRef.set(false);
     }
 
-    @ModifyVariable(method = "tryBreakBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;postMine(Lnet/minecraft/world/World;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;)V"), ordinal = 1)
-    private boolean modifyEffectiveTool(boolean original) {
-        for (ModifyHarvestPower mhp : PowerHolderComponent.getPowers(player, ModifyHarvestPower.class)) {
-            if (mhp.doesApply(savedBlockPosition)) {
-                return mhp.isHarvestAllowed();
-            }
-        }
-        return original;
+    @ModifyExpressionValue(method = "tryBreakBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;canHarvest(Lnet/minecraft/block/BlockState;)Z"))
+    private boolean apoli$modifyEffectiveTool(boolean original, @Share("cachedMinedBlock") LocalRef<SavedBlockPosition> cachedMinedBlockRef, @Share("modifiedCanHarvest") LocalBooleanRef modifiedCanHarvestRef) {
+
+        boolean result = PowerHolderComponent.getPowers(this.player, ModifyHarvestPower.class)
+            .stream()
+            .filter(mhp -> mhp.doesApply(cachedMinedBlockRef.get()))
+            .max(ModifyHarvestPower::compareTo)
+            .map(ModifyHarvestPower::isHarvestAllowed)
+            .orElse(original);
+
+        modifiedCanHarvestRef.set(result);
+        return result;
+
     }
 
-    @Inject(method = "tryBreakBlock", at = @At(value = "RETURN", ordinal = 4, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void actionOnBlockBreak(BlockPos pos, CallbackInfoReturnable<Boolean> cir, BlockState blockState, BlockEntity blockEntity, Block block, boolean bl, ItemStack itemStack, ItemStack itemStack2, boolean bl2) {
-        PowerHolderComponent.getPowers(player, ActionOnBlockBreakPower.class).stream().filter(p -> p.doesApply(savedBlockPosition))
-            .forEach(aobbp -> aobbp.executeActions(bl && bl2, pos, null));
+    @Inject(method = "tryBreakBlock", at = {@At(value = "RETURN", ordinal = 3), @At(value = "RETURN", ordinal = 4, shift = At.Shift.BEFORE)})
+    private void apoli$actionOnBlockBreak(BlockPos pos, CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 0) boolean blockRemoved, @Share("cachedMinedBlock") LocalRef<SavedBlockPosition> cachedMinedBlockRef, @Share("modifiedCanHarvest") LocalBooleanRef modifiedCanHarvestRef) {
+        boolean harvestedSuccessfully = blockRemoved && modifiedCanHarvestRef.get();
+        PowerHolderComponent.withPowers(this.player, ActionOnBlockBreakPower.class,
+            aobbp -> aobbp.doesApply(cachedMinedBlockRef.get()),
+            aobbp -> aobbp.executeActions(harvestedSuccessfully, pos, null));
     }
-
 
     @Inject(method = "interactBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;shouldCancelInteraction()Z"), cancellable = true)
-    private void preventBlockInteraction(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
-        if(PowerHolderComponent.getPowers(player, PreventBlockUsePower.class).stream().anyMatch(p -> p.doesPrevent(world, hitResult.getBlockPos()))) {
+    private void apoli$preventBlockInteraction(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
+        if (PowerHolderComponent.hasPower(player, PreventBlockUsePower.class, pbup -> pbup.doesPrevent(world, hitResult.getBlockPos()))) {
             cir.setReturnValue(ActionResult.FAIL);
         }
     }
 
-    @Inject(method = "interactBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;copy()Lnet/minecraft/item/ItemStack;"), cancellable = true)
-    private void executeBlockUseActions(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
-        PowerHolderComponent.getPowers(player, ActionOnBlockUsePower.class).stream()
-            .filter(p -> p.shouldExecute(hitResult.getBlockPos(), hitResult.getSide(), hand, stack))
-            .forEach(p -> p.executeAction(hitResult.getBlockPos(), hitResult.getSide(), hand));
+    @Inject(method = "interactBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;copy()Lnet/minecraft/item/ItemStack;"))
+    private void apoli$executeBlockUseActions(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
+        PowerHolderComponent.withPowers(this.player, ActionOnBlockUsePower.class,
+            aobbp -> aobbp.shouldExecute(hitResult, hand, stack),
+            aobbp -> aobbp.executeAction(hitResult, hand));
     }
+
 }
--- src/main/java/io/github/apace100/apoli/power/ModifyHarvestPower.java
+++ src/main/java/io/github/apace100/apoli/power/ModifyHarvestPower.java
@@ -3,33 +3,46 @@ package io.github.apace100.apoli.power;
 import io.github.apace100.apoli.Apoli;
 import io.github.apace100.apoli.data.ApoliDataTypes;
 import io.github.apace100.apoli.power.factory.PowerFactory;
-import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.block.pattern.CachedBlockPosition;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Predicate;
 
-public class ModifyHarvestPower extends Power {
+public class ModifyHarvestPower extends Power implements Prioritized<ModifyHarvestPower>, Comparable<ModifyHarvestPower> {
 
-    private final Predicate<CachedBlockPosition> predicate;
-    private boolean allow;
+    private final Predicate<CachedBlockPosition> blockCondition;
 
-    public ModifyHarvestPower(PowerType<?> type, LivingEntity entity, Predicate<CachedBlockPosition> predicate, boolean allow) {
+    private final boolean allow;
+    private final int priority;
+
+    public ModifyHarvestPower(PowerType<?> type, LivingEntity entity, Predicate<CachedBlockPosition> predicate, boolean allow, int priority) {
         super(type, entity);
-        this.predicate = predicate;
+        this.blockCondition = predicate;
         this.allow = allow;
+        this.priority = priority;
+    }
+
+    @Override
+    public int compareTo(@NotNull ModifyHarvestPower other) {
+        int priorityResult = Integer.compare(this.getPriority(), other.getPriority());
+        return priorityResult != 0 ? priorityResult : Boolean.compare(this.isHarvestAllowed(), other.isHarvestAllowed());
+    }
+
+    @Override
+    public int getPriority() {
+        return priority;
     }
 
     public boolean doesApply(BlockPos pos) {
-        CachedBlockPosition cbp = new CachedBlockPosition(entity.getWorld(), pos, true);
-        return predicate.test(cbp);
+        return doesApply(new CachedBlockPosition(entity.getWorld(), pos, true));
     }
 
     public boolean doesApply(CachedBlockPosition pos) {
-        return predicate.test(pos);
+        return blockCondition == null || blockCondition.test(pos);
     }
 
     public boolean isHarvestAllowed() {
@@ -37,15 +50,20 @@ public class ModifyHarvestPower extends Power {
     }
 
     public static PowerFactory createFactory() {
-        return new PowerFactory<>(Apoli.identifier("modify_harvest"),
+        return new PowerFactory<>(
+            Apoli.identifier("modify_harvest"),
             new SerializableData()
                 .add("block_condition", ApoliDataTypes.BLOCK_CONDITION, null)
-                .add("allow", SerializableDataTypes.BOOLEAN),
-            data ->
-                (type, player) ->
-                    new ModifyHarvestPower(type, player,
-                        data.isPresent("block_condition") ? (ConditionFactory<CachedBlockPosition>.Instance)data.get("block_condition") : cbp -> true,
-                        data.getBoolean("allow")))
-            .allowCondition();
+                .add("allow", SerializableDataTypes.BOOLEAN)
+                .add("priority", SerializableDataTypes.INT, 0),
+            data -> (powerType, livingEntity) -> new ModifyHarvestPower(
+                powerType,
+                livingEntity,
+                data.get("block_condition"),
+                data.get("allow"),
+                data.get("priority")
+            )
+        ).allowCondition();
     }
+
 }
