bb1d29ac499345699d950d78fbeecb2c0817726e
eggohito <66972816+eggohito@users.noreply.github.com>
2023-09-20 01:17:57 +0800

    Added `game_event_listener` power type

---
+++ src/main/java/io/github/apace100/apoli/power/GameEventListenerPower.java
@@ -0,0 +1,215 @@
+package io.github.apace100.apoli.power;
+
+import io.github.apace100.apoli.Apoli;
+import io.github.apace100.apoli.access.PowerLinkedListenerData;
+import io.github.apace100.apoli.data.ApoliDataTypes;
+import io.github.apace100.apoli.power.factory.PowerFactory;
+import io.github.apace100.apoli.util.HudRender;
+import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
+import net.minecraft.block.pattern.CachedBlockPosition;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.registry.tag.GameEventTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Pair;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.event.EntityPositionSource;
+import net.minecraft.world.event.GameEvent;
+import net.minecraft.world.event.PositionSource;
+import net.minecraft.world.event.Vibrations;
+import net.minecraft.world.event.listener.EntityGameEventHandler;
+import org.apache.commons.lang3.tuple.Triple;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class GameEventListenerPower extends CooldownPower implements Vibrations {
+
+    private final Consumer<Triple<World, BlockPos, Direction>> blockAction;
+    private final Consumer<Pair<Entity, Entity>> biEntityAction;
+
+    private final Predicate<CachedBlockPosition> blockCondition;
+    private final Predicate<Pair<Entity, Entity>> biEntityCondition;
+
+    private final List<GameEvent> acceptedGameEvents;
+    private final TagKey<GameEvent> acceptedGameEventTag;
+
+    private EntityGameEventHandler<VibrationListener> gameEventHandler;
+    private final ListenerData vibrationListenerData;
+    private final Callback vibrationCallback;
+
+    private final boolean showParticle;
+    private final int range;
+
+    public GameEventListenerPower(PowerType<?> powerType, LivingEntity livingEntity, Consumer<Triple<World, BlockPos, Direction>> blockAction, Consumer<Pair<Entity, Entity>> biEntityAction, Predicate<CachedBlockPosition> blockCondition, Predicate<Pair<Entity, Entity>> biEntityCondition, int cooldownDuration, HudRender hudRender, int range, GameEvent acceptedGameEvent, List<GameEvent> acceptedGameEvents, TagKey<GameEvent> acceptedGameEventTag, boolean showParticle) {
+        super(powerType, livingEntity, Math.max(cooldownDuration, 1), hudRender);
+
+        this.blockAction = blockAction;
+        this.biEntityAction = biEntityAction;
+        this.blockCondition = blockCondition;
+        this.biEntityCondition = biEntityCondition;
+        this.range = Math.max(range, 1);
+
+        this.acceptedGameEvents = new LinkedList<>();
+        if (acceptedGameEvent != null) {
+            this.acceptedGameEvents.add(acceptedGameEvent);
+        }
+        if (acceptedGameEvents != null) {
+            this.acceptedGameEvents.addAll(acceptedGameEvents);
+        }
+
+        this.gameEventHandler = null;
+        this.vibrationListenerData = new ListenerData();
+        this.vibrationCallback = new Callback();
+
+        this.acceptedGameEventTag = acceptedGameEventTag;
+        this.showParticle = showParticle;
+        this.setTicking();
+
+    }
+
+    @Override
+    public void onAdded() {
+        if (!entity.getWorld().isClient) {
+            entity.updateEventHandler(EntityGameEventHandler::onEntitySetPos);
+        }
+    }
+
+    @Override
+    public void onRemoved() {
+        if (!entity.getWorld().isClient) {
+            entity.updateEventHandler(EntityGameEventHandler::onEntityRemoval);
+        }
+    }
+
+    @Override
+    public void tick() {
+        if (canUse()) {
+            Ticker.tick(entity.getWorld(), getVibrationListenerData(), getVibrationCallback());
+        }
+    }
+
+    @Override
+    public boolean canUse() {
+        return canListen() && super.canUse();
+    }
+
+    @Override
+    public ListenerData getVibrationListenerData() {
+        ((PowerLinkedListenerData) vibrationListenerData).apoli$setPower(this);
+        return vibrationListenerData;
+    }
+
+    @Override
+    public Callback getVibrationCallback() {
+        return vibrationCallback;
+    }
+
+    public boolean canListen() {
+        return gameEventHandler != null
+            && gameEventHandler.getListener() != null;
+    }
+
+    public EntityGameEventHandler<VibrationListener> getGameEventHandler() {
+
+        if (!canListen()) {
+            gameEventHandler = new EntityGameEventHandler<>(new VibrationListener(this));
+        }
+
+        return gameEventHandler;
+
+    }
+
+    public boolean shouldShowParticle() {
+        return showParticle;
+    }
+
+    public class Callback implements Vibrations.Callback {
+
+        @Override
+        public int getRange() {
+            return range;
+        }
+
+        @Override
+        public PositionSource getPositionSource() {
+            return new EntityPositionSource(entity, entity.getEyeHeight(entity.getPose()));
+        }
+
+        @Override
+        public boolean accepts(ServerWorld world, BlockPos pos, GameEvent event, GameEvent.Emitter emitter) {
+            return entity.getWorld() == world
+                && (blockCondition == null || blockCondition.test(new CachedBlockPosition(world, pos, true)))
+                && (biEntityCondition == null || biEntityCondition.test(new Pair<>(emitter.sourceEntity(), entity)));
+        }
+
+        @Override
+        public void accept(ServerWorld world, BlockPos pos, GameEvent event, @Nullable Entity sourceEntity, @Nullable Entity actor, float distance) {
+
+            use();
+            if (blockAction != null) {
+                blockAction.accept(Triple.of(world, pos, Direction.UP));
+            }
+
+            if (biEntityAction != null) {
+                biEntityAction.accept(new Pair<>(actor, entity));
+            }
+
+        }
+
+        @Override
+        public boolean canAccept(GameEvent gameEvent, GameEvent.Emitter emitter) {
+            return canUse()
+                && Vibrations.Callback.super.canAccept(gameEvent, emitter)
+                && (acceptedGameEvents.isEmpty() || acceptedGameEvents.contains(gameEvent));
+        }
+
+        @Override
+        public TagKey<GameEvent> getTag() {
+            return acceptedGameEventTag;
+        }
+
+    }
+
+    public static PowerFactory createFactory() {
+        return new PowerFactory<>(
+            Apoli.identifier("game_event_listener"),
+            new SerializableData()
+                .add("block_action", ApoliDataTypes.BLOCK_ACTION, null)
+                .add("bientity_action", ApoliDataTypes.BIENTITY_ACTION, null)
+                .add("block_condition", ApoliDataTypes.BLOCK_CONDITION, null)
+                .add("bientity_condition", ApoliDataTypes.BIENTITY_CONDITION, null)
+                .add("cooldown", SerializableDataTypes.INT, 1)
+                .add("hud_render", ApoliDataTypes.HUD_RENDER, HudRender.DONT_RENDER)
+                .add("range", SerializableDataTypes.INT, 16)
+                .add("event", SerializableDataTypes.GAME_EVENT, null)
+                .add("events", SerializableDataTypes.GAME_EVENTS, null)
+                .add("tag", SerializableDataTypes.GAME_EVENT_TAG, GameEventTags.VIBRATIONS)
+                .addFunctionedDefault("event_tag", SerializableDataTypes.GAME_EVENT_TAG, data -> data.get("tag"))
+                .add("show_particle", SerializableDataTypes.BOOLEAN, true),
+            data -> (powerType, livingEntity) -> new GameEventListenerPower(
+                powerType,
+                livingEntity,
+                data.get("block_action"),
+                data.get("bientity_action"),
+                data.get("block_condition"),
+                data.get("bientity_condition"),
+                data.get("cooldown"),
+                data.get("hud_render"),
+                data.get("range"),
+                data.get("event"),
+                data.get("events"),
+                data.get("event_tag"),
+                data.get("show_particle")
+            )
+        ).allowCondition();
+    }
+
+}
