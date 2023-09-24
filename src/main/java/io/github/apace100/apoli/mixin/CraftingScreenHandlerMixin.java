e8b671114273398b279c133a0d1552b9a98aaf43
eggohito <66972816+eggohito@users.noreply.github.com>
2023-09-24 22:54:30 +0800

    Tweaked `CraftingScreenHandlerMixin` * Used MixinExtras wherever possible

--- src/main/java/io/github/apace100/apoli/mixin/CraftingScreenHandlerMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/CraftingScreenHandlerMixin.java
@@ -1,5 +1,6 @@
 package io.github.apace100.apoli.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import io.github.apace100.apoli.access.PowerCraftingInventory;
 import io.github.apace100.apoli.power.ModifyCraftingPower;
 import net.minecraft.entity.player.PlayerEntity;
@@ -29,23 +30,33 @@ public class CraftingScreenHandlerMixin {
     @Shadow @Final private RecipeInputInventory input;
 
     @Inject(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/RecipeManager;getFirstMatch(Lnet/minecraft/recipe/RecipeType;Lnet/minecraft/inventory/Inventory;Lnet/minecraft/world/World;)Ljava/util/Optional;"))
-    private static void clearPowerCraftingInventory(ScreenHandler handler, World world, PlayerEntity player, RecipeInputInventory inventory, CraftingResultInventory resultInventory, CallbackInfo ci) {
-        if (inventory instanceof CraftingInventory craftingInventory) ((PowerCraftingInventory)craftingInventory).apoli$setPower(null);
+    private static void apoli$clearPowerCraftingInventory(ScreenHandler handler, World world, PlayerEntity player, RecipeInputInventory inventory, CraftingResultInventory resultInventory, CallbackInfo ci) {
+
+        if (inventory instanceof CraftingInventory craftingInventory) {
+            ((PowerCraftingInventory) craftingInventory).apoli$setPower(null);
+        }
+
     }
 
-    @Inject(method = "canUse", at = @At("HEAD"), cancellable = true)
-    private void allowUsingViaPower(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
-        if(context.get((world, pos) -> pos.equals(player.getBlockPos()), false)) {
-            cir.setReturnValue(true);
-        }
+    @ModifyReturnValue(method = "canUse", at = @At("RETURN"))
+    private boolean apoli$allowUsingViaPower(boolean original, PlayerEntity playerEntity) {
+        return original || context.get((world, pos) -> pos.equals(playerEntity.getBlockPos()), false);
     }
 
     @Inject(method = "quickMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;copy()Lnet/minecraft/item/ItemStack;", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void modifyOutputItems(PlayerEntity player, int index, CallbackInfoReturnable<ItemStack> cir, ItemStack itemStack, Slot slot, ItemStack itemStack2) {
+    private void apoli$modifyResultStack(PlayerEntity player, int index, CallbackInfoReturnable<ItemStack> cir, ItemStack itemStack, Slot slot, ItemStack itemStack2) {
+
+        /*
+            FIXME: Currently, this does not account for whether the item can be inserted into the player's inventory,
+                   resulting in the item action being executed
+        */
         if(input instanceof PowerCraftingInventory pci) {
+
             if(pci.apoli$getPower() instanceof ModifyCraftingPower mcp) {
                 mcp.applyAfterCraftingItemAction(itemStack2);
             }
+
         }
+
     }
 }
