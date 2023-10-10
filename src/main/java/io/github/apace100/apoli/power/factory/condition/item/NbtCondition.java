e20ec2486d39f40425f04ba449499aa97b183fa9
eggohito <66972816+eggohito@users.noreply.github.com>
2023-10-10 21:21:30 +0800

    Fixed `nbt` item condition type causing a crash

--- src/main/java/io/github/apace100/apoli/power/factory/condition/item/NbtCondition.java
+++ src/main/java/io/github/apace100/apoli/power/factory/condition/item/NbtCondition.java
@@ -3,6 +3,7 @@ package io.github.apace100.apoli.power.factory.condition.item;
 import io.github.apace100.apoli.Apoli;
 import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
 import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtHelper;
 import net.minecraft.util.Pair;
@@ -17,7 +18,8 @@ public class NbtCondition {
     public static ConditionFactory<Pair<World, ItemStack>> getFactory() {
         return new ConditionFactory<>(
             Apoli.identifier("nbt"),
-            new SerializableData(),
+            new SerializableData()
+                .add("nbt", SerializableDataTypes.NBT),
             NbtCondition::condition
         );
     }
