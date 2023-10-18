a0cb0dda1f3de6a8df951fe3527ba60388d14275
eggohito <66972816+eggohito@users.noreply.github.com>
2023-10-18 18:22:09 +0800

    Fixed issue with `crafting_table` entity action type * The crafting table interface will now remain even if the player is moving

--- src/main/java/io/github/apace100/apoli/mixin/CraftingScreenHandlerMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/CraftingScreenHandlerMixin.java
@@ -3,6 +3,7 @@ package io.github.apace100.apoli.mixin;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import io.github.apace100.apoli.access.PowerCraftingInventory;
+import io.github.apace100.apoli.access.ScreenHandlerUsabilityOverride;
 import io.github.apace100.apoli.access.SlotState;
 import io.github.apace100.apoli.power.ModifyCraftingPower;
 import net.minecraft.entity.player.PlayerEntity;
@@ -10,27 +11,45 @@ import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.CraftingResultInventory;
 import net.minecraft.inventory.RecipeInputInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.screen.AbstractRecipeScreenHandler;
 import net.minecraft.screen.CraftingScreenHandler;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.slot.CraftingResultSlot;
 import net.minecraft.screen.slot.Slot;
-import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CraftingScreenHandler.class)
-public class CraftingScreenHandlerMixin {
+public abstract class CraftingScreenHandlerMixin extends AbstractRecipeScreenHandler<RecipeInputInventory> implements ScreenHandlerUsabilityOverride {
 
-    @Shadow @Final private ScreenHandlerContext context;
+    @Shadow
+    @Final
+    private RecipeInputInventory input;
 
-    @Shadow @Final private RecipeInputInventory input;
+    @Unique
+    private boolean apoli$canUse = false;
+
+    @Override
+    public boolean apoli$canUse() {
+        return this.apoli$canUse;
+    }
+
+    @Override
+    public void apoli$canUse(boolean canUse) {
+        this.apoli$canUse = canUse;
+    }
+
+    public CraftingScreenHandlerMixin(ScreenHandlerType<?> screenHandlerType, int i) {
+        super(screenHandlerType, i);
+    }
 
     @Inject(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/RecipeManager;getFirstMatch(Lnet/minecraft/recipe/RecipeType;Lnet/minecraft/inventory/Inventory;Lnet/minecraft/world/World;)Ljava/util/Optional;"))
     private static void apoli$clearPowerCraftingInventory(ScreenHandler handler, World world, PlayerEntity player, RecipeInputInventory inventory, CraftingResultInventory resultInventory, CallbackInfo ci) {
@@ -43,7 +62,7 @@ public class CraftingScreenHandlerMixin {
 
     @ModifyReturnValue(method = "canUse", at = @At("RETURN"))
     private boolean apoli$allowUsingViaPower(boolean original, PlayerEntity playerEntity) {
-        return original || context.get((world, pos) -> pos.equals(playerEntity.getBlockPos()), false);
+        return original || this.apoli$canUse();
     }
 
     @Inject(method = "quickMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;copy()Lnet/minecraft/item/ItemStack;", shift = At.Shift.AFTER))
