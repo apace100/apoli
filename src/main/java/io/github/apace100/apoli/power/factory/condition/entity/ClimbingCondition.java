38f95c899da4552c8904f930028887184bcc3445
eggohito <66972816+eggohito@users.noreply.github.com>
2023-11-02 21:21:43 +0800

    Added `current` field to `climbing` entity condition type

--- src/main/java/io/github/apace100/apoli/power/factory/condition/entity/ClimbingCondition.java
+++ src/main/java/io/github/apace100/apoli/power/factory/condition/entity/ClimbingCondition.java
@@ -4,19 +4,22 @@ import io.github.apace100.apoli.Apoli;
 import io.github.apace100.apoli.access.MovingEntity;
 import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
 import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 
 public class ClimbingCondition {
 
     public static boolean condition(SerializableData.Instance data, Entity entity) {
-        return entity instanceof LivingEntity livingEntity && ((MovingEntity) livingEntity).apoli$activelyClimbing();
+        return entity instanceof LivingEntity livingEntity
+            && (data.getBoolean("current") ? ((MovingEntity) livingEntity).apoli$activelyClimbing() : livingEntity.isClimbing());
     }
 
     public static ConditionFactory<Entity> getFactory() {
         return new ConditionFactory<>(
             Apoli.identifier("climbing"),
-            new SerializableData(),
+            new SerializableData()
+                .add("current", SerializableDataTypes.BOOLEAN, true),
             ClimbingCondition::condition
         );
     }
