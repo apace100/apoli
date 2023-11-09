f2b7f615b1c5effcfb0b4206ad5618dd57472b1b
eggohito <66972816+eggohito@users.noreply.github.com>
2023-11-09 15:45:41 +0800

    Added `chance` meta condition type

--- src/main/java/io/github/apace100/apoli/power/factory/condition/MetaConditions.java
+++ src/main/java/io/github/apace100/apoli/power/factory/condition/MetaConditions.java
@@ -1,6 +1,7 @@
 package io.github.apace100.apoli.power.factory.condition;
 
 import io.github.apace100.apoli.power.factory.condition.meta.AndCondition;
+import io.github.apace100.apoli.power.factory.condition.meta.ChanceCondition;
 import io.github.apace100.apoli.power.factory.condition.meta.ConstantCondition;
 import io.github.apace100.apoli.power.factory.condition.meta.OrCondition;
 import io.github.apace100.calio.data.SerializableDataType;
@@ -13,6 +14,7 @@ public class MetaConditions {
         registrant.accept(AndCondition.getFactory(dataType));
         registrant.accept(ConstantCondition.getFactory());
         registrant.accept(OrCondition.getFactory(dataType));
+        registrant.accept(ChanceCondition.getFactory());
     }
 
 }
