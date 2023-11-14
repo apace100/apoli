7a842f3e2b82c43074e006464ad992ab0ee2c306
eggohito <66972816+eggohito@users.noreply.github.com>
2023-11-14 18:55:00 +0800

    Fixed spawnpoint not persisting upon being set

--- src/main/java/io/github/apace100/apoli/mixin/LoginMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/LoginMixin.java
@@ -9,7 +9,6 @@ import io.github.apace100.apoli.power.ModifyPlayerSpawnPower;
 import io.github.apace100.apoli.power.Power;
 import net.minecraft.entity.Dismounting;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -21,7 +20,6 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -31,8 +29,8 @@ import java.util.Optional;
 public abstract class LoginMixin {
 
 	@WrapOperation(method = "respawnPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;setSpawnPoint(Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/util/math/BlockPos;FZZ)V"))
-	private void apoli$preventEndExitSpawnPointSetting(ServerPlayerEntity instance, RegistryKey<World> dimension, @Nullable BlockPos pos, float angle, boolean forced, boolean sendMessage, Operation<Void> original) {
-		if (((EndRespawningEntity) instance).apoli$hasRealRespawnPoint()) {
+	private void apoli$preventEndExitSpawnPointSetting(ServerPlayerEntity instance, RegistryKey<World> dimension, @Nullable BlockPos pos, float angle, boolean forced, boolean sendMessage, Operation<Void> original, ServerPlayerEntity mPlayer, boolean mAlive) {
+		if (((EndRespawningEntity) mPlayer).apoli$hasRealRespawnPoint()) {
 			original.call(instance, dimension, pos, angle, forced, sendMessage);
 		}
 	}
@@ -45,15 +43,15 @@ public abstract class LoginMixin {
 		});
 	}
 
-	@Redirect(method = "respawnPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;findRespawnPosition(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;FZZ)Ljava/util/Optional;"))
-	private Optional<Vec3d> apoli$retryObstructedSpawnpointIfFailed(ServerWorld world, BlockPos pos, float f, boolean bl, boolean bl2, ServerPlayerEntity player, boolean alive) {
+	@WrapOperation(method = "respawnPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;findRespawnPosition(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;FZZ)Ljava/util/Optional;"))
+	private Optional<Vec3d> apoli$retryObstructedSpawnpointIfFailed(ServerWorld world, BlockPos pos, float angle, boolean forced, boolean alive, Operation<Optional<Vec3d>> original, ServerPlayerEntity mPlayer) {
 
-		Optional<Vec3d> original = PlayerEntity.findRespawnPosition(world, pos, f, bl, bl2);
-		if (original.isEmpty() && PowerHolderComponent.hasPower(player, ModifyPlayerSpawnPower.class)) {
-			return Optional.ofNullable(Dismounting.findRespawnPos(EntityType.PLAYER, world, pos, bl));
+		Optional<Vec3d> originalPosition = original.call(world, pos, angle, forced, alive);
+		if (originalPosition.isEmpty() && PowerHolderComponent.hasPower(mPlayer, ModifyPlayerSpawnPower.class)) {
+			return Optional.ofNullable(Dismounting.findRespawnPos(EntityType.PLAYER, world, pos, forced));
 		}
 
-		return original;
+		return originalPosition;
 
 	}
 
