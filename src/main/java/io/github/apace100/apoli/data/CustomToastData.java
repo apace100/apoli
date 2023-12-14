fbf3b03b634b155cc655a429f07dd8d65785e2cb
eggohito <66972816+eggohito@users.noreply.github.com>
2023-12-14 18:07:16 +0800

    Tweaked custom toasts + Added `texture` field; used for specifying a custom texture for the custom toast - Removed `sound_event` field, as it isn't really necessary * The size of the toast now depends on the length of the description

--- src/main/java/io/github/apace100/apoli/data/CustomToastData.java
+++ src/main/java/io/github/apace100/apoli/data/CustomToastData.java
@@ -1,28 +1,30 @@
 package io.github.apace100.apoli.data;
 
+import io.github.apace100.apoli.Apoli;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataType;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.util.Identifier;
 
-public record CustomToastData(Text title, Text description, ItemStack iconStack, @Nullable SoundEvent soundEvent, int duration) {
+public record CustomToastData(Text title, Text description, Identifier texture, ItemStack iconStack, int duration) {
+
+    public static final Identifier DEFAULT_TEXTURE = Apoli.identifier("toast/custom");
 
     public static final SerializableDataType<CustomToastData> DATA_TYPE = SerializableDataType.compound(
         CustomToastData.class,
         new SerializableData()
             .add("title", SerializableDataTypes.TEXT)
             .add("description", SerializableDataTypes.TEXT)
+            .add("texture", SerializableDataTypes.IDENTIFIER, DEFAULT_TEXTURE)
             .add("icon", SerializableDataTypes.ITEM_STACK, ItemStack.EMPTY)
-            .add("sound_event", SerializableDataTypes.SOUND_EVENT, null)
             .add("duration", SerializableDataTypes.POSITIVE_INT, 5000),
         data -> new CustomToastData(
             data.get("title"),
             data.get("description"),
+            data.get("texture"),
             data.get("icon"),
-            data.get("sound_event"),
             data.get("duration")
         ),
         (serializableData, toastData) -> {
@@ -31,8 +33,8 @@ public record CustomToastData(Text title, Text description, ItemStack iconStack,
 
             data.set("title", toastData.title);
             data.set("description", toastData.description);
+            data.set("texture", toastData.texture);
             data.set("icon", toastData.iconStack);
-            data.set("sound_event", toastData.soundEvent);
             data.set("duration", toastData.duration);
 
             return data;
--- src/main/java/io/github/apace100/apoli/screen/toast/CustomToast.java
+++ src/main/java/io/github/apace100/apoli/screen/toast/CustomToast.java
@@ -1,97 +1,101 @@
 package io.github.apace100.apoli.screen.toast;
 
+import io.github.apace100.apoli.Apoli;
 import io.github.apace100.apoli.data.CustomToastData;
+import io.github.apace100.apoli.util.TextureUtil;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class CustomToast implements Toast {
 
-    private static final Identifier TEXTURE = new Identifier("toast/advancement");
-
     private static final int TITLE_BASE_COLOR = 16776960;
     private static final int DESCRIPTION_BASE_COLOR = 16777215;
-    private static final int ALPHA_SHIFT_END = 1500;
 
     private final Text title;
     private final List<OrderedText> description;
     private final ItemStack iconStack;
+    private final Identifier texture;
 
-    @Nullable private final SoundEvent soundEvent;
-
+    private final int alphaShiftEnd;
     private final int duration;
-    private boolean playedSound = false;
 
     public CustomToast(CustomToastData toastData) {
-        this(toastData.title(), toastData.description(), toastData.iconStack(), toastData.soundEvent(), toastData.duration());
+        this(toastData.title(), toastData.description(), toastData.texture(), toastData.iconStack(), toastData.duration());
     }
 
-    public CustomToast(Text title, Text description, ItemStack iconStack, @Nullable SoundEvent soundEvent, int duration) {
+    public CustomToast(Text title, Text description, Identifier texture, ItemStack iconStack, int duration) {
         this.title = title;
-        this.description = MinecraftClient.getInstance().textRenderer.wrapLines(description, this.getWidth() - 13);
+        this.description = MinecraftClient.getInstance().textRenderer.wrapLines(description, this.getWidth() - 33);
         this.iconStack = iconStack;
-        this.soundEvent = soundEvent;
+        this.texture = TextureUtil.tryLoadingSprite(texture, TextureUtil.GUI_ATLAS_TEXTURE)
+            .resultOrPartial(err -> {
+                Apoli.LOGGER.error(err);
+                Apoli.LOGGER.warn("Using default texture (\"{}\") instead...", CustomToastData.DEFAULT_TEXTURE);
+            })
+            .orElse(CustomToastData.DEFAULT_TEXTURE);
+        this.alphaShiftEnd = duration / 3;
         this.duration = duration;
     }
 
+    //  TODO:   Handle long titles, like how long descriptions are handled
     @Override
     public Visibility draw(DrawContext context, ToastManager manager, long startTime) {
 
-        //  Draw the texture for the toast
         TextRenderer textRenderer = manager.getClient().textRenderer;
-        context.drawGuiTexture(TEXTURE, 0, 0, this.getWidth(), this.getHeight());
 
-        int alphaShift = MathHelper.floor(MathHelper.clamp((float) Math.abs(ALPHA_SHIFT_END - startTime) / 300, 0.0, 1.0) * 255.0f) << 24 | 67108864;
+        int alphaShift = MathHelper.floor(MathHelper.clamp((float) Math.abs(alphaShiftEnd - startTime) / 300, 0.0, 1.0) * 255.0f) << 24 | 67108864;
+        int centeredY = this.getHeight() / 2;
+
+        int x = 30;
+        int descriptionY = centeredY - description.size() * 9 / 2;
+
+        //  Draw the texture and icon of the toast
+        context.drawGuiTexture(texture, 0, 0, this.getWidth(), this.getHeight());
+        context.drawItemWithoutEntity(iconStack, 8, centeredY - 8);
 
         //  If the description text is only 1 line, display it as is
         if (description.size() == 1) {
-            context.drawText(textRenderer, title, 30, 7, TITLE_BASE_COLOR | 0xFF000000, false);
-            context.drawText(textRenderer, description.get(0), 28, 18, -1, false);
+            context.drawText(textRenderer, title, x, 7, TITLE_BASE_COLOR | 0xFF000000, false);
+            context.drawText(textRenderer, description.get(0), x, 18, -1, false);
         }
 
         //  Else, if the drawing process is currently within the shifting phase, shift the alpha of the title text
-        else if (startTime < ALPHA_SHIFT_END) {
-            context.drawText(textRenderer, title, 30, 11, TITLE_BASE_COLOR | alphaShift, false);
+        else if (startTime < alphaShiftEnd) {
+            context.drawText(textRenderer, title, x, centeredY - 8, TITLE_BASE_COLOR | alphaShift, false);
         }
 
         //  Otherwise, fit the description text into the toast window
-        //  TODO: Make the toast expand horizontally if the description and title is too long (e.g: more than 1 line)
         else {
 
-            int yOffset = Math.max(7, this.getHeight() / 2 - description.size() * 9 / 2);
+            int yOffset = Math.max(7, descriptionY);
 
             for (OrderedText descriptionLine : description) {
-                context.drawText(textRenderer, descriptionLine, 28, yOffset, DESCRIPTION_BASE_COLOR | alphaShift, false);
+                context.drawText(textRenderer, descriptionLine, x, yOffset, DESCRIPTION_BASE_COLOR | alphaShift, false);
                 yOffset += 9;
             }
 
         }
 
-        //  If a sound event is specified, play it once
-        if (soundEvent != null && !this.playedSound && startTime > 0) {
-            this.playedSound = true;
-            manager.getClient().getSoundManager().play(PositionedSoundInstance.master(soundEvent, 1.0f, 1.0f));
-        }
-
-        //  Draw the item stack used as the icon
-        context.drawItemWithoutEntity(iconStack, 8, 8);
         return startTime >= duration * manager.getNotificationDisplayTimeMultiplier() ? Visibility.HIDE : Visibility.SHOW;
 
+    }
+
+    @Override
+    public int getHeight() {
+        return 24 + Math.min(Math.max(this.description.size(), 1), 3) * 8;
     }
 
 }
