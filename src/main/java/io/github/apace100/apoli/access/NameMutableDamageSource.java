a4099e309b4df664616765586261d7fab805bfe7
eggohito <66972816+eggohito@users.noreply.github.com>
2024-03-23 15:38:21 +0800

    Tweaked impl. of `DamageSourceDescription` + Now accounts for the message ID when searching for the best matching damage type * Now actually overrides the name of the damage source with the set name (Resolves #207)

--- src/main/java/io/github/apace100/apoli/access/NameMutableDamageSource.java
+++ src/main/java/io/github/apace100/apoli/access/NameMutableDamageSource.java
@@ -1,6 +1,10 @@
 package io.github.apace100.apoli.access;
 
+import java.util.Optional;
+
 public interface NameMutableDamageSource {
+
+    Optional<String> apoli$getName();
 
     void apoli$setName(String name);
 
