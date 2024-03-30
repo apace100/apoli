26811df7577444910223bd59e3030dc2f633716a
eggohito <66972816+eggohito@users.noreply.github.com>
2024-03-31 04:40:25 +0800

    Fixed spyglass FOV not being affected by the `modify_fov` power type

--- src/main/java/io/github/apace100/apoli/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/AbstractClientPlayerEntityMixin.java
@@ -1,6 +1,6 @@
 package io.github.apace100.apoli.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.authlib.GameProfile;
@@ -9,7 +9,6 @@ import io.github.apace100.apoli.power.ModifyFovPower;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,27 +22,23 @@ public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
         super(world, pos, yaw, gameProfile);
     }
 
-    @ModifyExpressionValue(method = "getFovMultiplier", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;isUsingSpyglass()Z"))
-    private boolean apoli$delegateSpyglassFovMultiplier(boolean original) {
-        return !PowerHolderComponent.hasPower(this, ModifyFovPower.class) && original;
+    @ModifyReturnValue(method = "getFovMultiplier", at = @At(value = "RETURN", ordinal = 0))
+    private float apoli$modifySpyglassFov(float original) {
+        return PowerHolderComponent.modify(this, ModifyFovPower.class, original);
     }
 
     @WrapOperation(method = "getFovMultiplier", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F"))
     private float apoli$modifyFov(float delta, float start, float end, Operation<Float> original) {
 
         List<ModifyFovPower> mfps = PowerHolderComponent.getPowers(this, ModifyFovPower.class);
-        if (mfps.isEmpty()) {
-            return original.call(delta, start, end);
-        }
-
-        boolean affectedByFovEffectScale = mfps
+        boolean affectedByFovEffectScale = mfps.isEmpty() || mfps
             .stream()
             .anyMatch(ModifyFovPower::isAffectedByFovEffectScale);
 
         float newEnd = PowerHolderComponent.modify(this, ModifyFovPower.class, end);
         float newDelta = affectedByFovEffectScale ? delta : start;
 
-        return MathHelper.lerp(newDelta, start, newEnd);
+        return original.call(newDelta, start, newEnd);
 
     }
 
