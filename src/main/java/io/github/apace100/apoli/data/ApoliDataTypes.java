d4c4472254ac0c86ec877ef73f636a9202d3f5e4
eggohito <66972816+eggohito@users.noreply.github.com>
2024-05-21 04:15:45 +0800

    Made the `*_block_use` power types consistent with the `*_entity_use` power types (#231)

--- src/main/java/io/github/apace100/apoli/data/ApoliDataTypes.java
+++ src/main/java/io/github/apace100/apoli/data/ApoliDataTypes.java
@@ -440,6 +440,10 @@ public class ApoliDataTypes {
         }
     );
 
+    public static final SerializableDataType<BlockUsagePhase> BLOCK_USAGE_PHASE = SerializableDataType.enumValue(BlockUsagePhase.class);
+
+    public static final SerializableDataType<EnumSet<BlockUsagePhase>> BLOCK_USAGE_PHASE_SET = SerializableDataType.enumSet(BlockUsagePhase.class, BLOCK_USAGE_PHASE);
+
     public static <T> SerializableDataType<ConditionFactory<T>.Instance> condition(Registry<ConditionFactory<T>> registry, String name) {
         return condition(registry, IdentifierAlias.GLOBAL, name);
     }
