83b0af03e06a0ba2b0dcc3514635e8f97c1fbb38
eggohito <66972816+eggohito@users.noreply.github.com>
2024-08-11 05:58:35 +0800

    Simplified impl. of `modify_type_tag` power type, etc. The following fields have been removed in favor of simplifying the power type. IMO, they aren't much use-cases to these, so I decided to remove them: - `replace` boolean field - `priority` int field
    This also fixes a tiny issue where it doesn't inject into the overload method for checking if the entity type is in a registry entry list, which is needed for other evaluating stuff

--- src/main/java/io/github/apace100/apoli/mixin/EntityTypeMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/EntityTypeMixin.java
@@ -5,6 +5,7 @@ import io.github.apace100.apoli.access.EntityLinkedType;
 import io.github.apace100.apoli.power.ModifyTypeTagPowerType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.tag.TagKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -27,8 +28,15 @@ public abstract class EntityTypeMixin implements EntityLinkedType {
     }
 
     @ModifyReturnValue(method = "isIn(Lnet/minecraft/registry/tag/TagKey;)Z", at = @At("RETURN"))
-    private boolean apoli$modifyTypeTag(boolean original, TagKey<EntityType<?>> tag) {
-        return ModifyTypeTagPowerType.doesApply(this.apoli$getEntity(), tag, original);
+    private boolean apoli$inTagProxy(boolean original, TagKey<EntityType<?>> tag) {
+        return original
+            || ModifyTypeTagPowerType.doesApply(this.apoli$getEntity(), tag);
+    }
+
+    @ModifyReturnValue(method = "isIn(Lnet/minecraft/registry/entry/RegistryEntryList;)Z", at = @At("RETURN"))
+    private boolean apoli$inTagEntryListProxy(boolean original, RegistryEntryList<EntityType<?>> entryList) {
+        return original
+            || ModifyTypeTagPowerType.doesApply(this.apoli$getEntity(), entryList);
     }
 
 }
--- src/main/java/io/github/apace100/apoli/power/ModifyTypeTagPowerType.java
+++ src/main/java/io/github/apace100/apoli/power/ModifyTypeTagPowerType.java
@@ -1,93 +1,45 @@
 package io.github.apace100.apoli.power;
 
 import io.github.apace100.apoli.Apoli;
+import io.github.apace100.apoli.component.PowerHolderComponent;
 import io.github.apace100.apoli.power.factory.PowerTypeFactory;
 import io.github.apace100.apoli.power.type.PowerType;
-import io.github.apace100.apoli.power.type.Prioritized;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.tag.TagKey;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 
-public class ModifyTypeTagPowerType extends PowerType implements Prioritized<ModifyTypeTagPowerType> {
+public class ModifyTypeTagPowerType extends PowerType {
 
     private final TagKey<EntityType<?>> tag;
 
-    private final boolean replace;
-    private final int priority;
-
-    public ModifyTypeTagPowerType(Power power, LivingEntity entity, TagKey<EntityType<?>> tag, boolean replace, int priority) {
+    public ModifyTypeTagPowerType(Power power, LivingEntity entity, TagKey<EntityType<?>> tag) {
         super(power, entity);
         this.tag = tag;
-        this.replace = replace;
-        this.priority = priority;
     }
 
-    @Override
-    public int getPriority() {
-        return priority;
+    public static boolean doesApply(Entity entity, TagKey<EntityType<?>> entityTypeTag) {
+        return PowerHolderComponent.hasPowerType(entity, ModifyTypeTagPowerType.class, p -> Objects.equals(p.tag, entityTypeTag));
     }
 
-    public TagKey<EntityType<?>> getTag() {
-        return tag;
-    }
-
-    public boolean matches(TagKey<EntityType<?>> otherTag) {
-        return this.getTag().equals(otherTag);
-    }
-
-    public boolean shouldReplace() {
-        return replace;
-    }
-
-    public static boolean doesApply(Entity entity, TagKey<EntityType<?>> entityTypeTag, boolean original) {
-
-        CallInstance<ModifyTypeTagPowerType> mttpci = new CallInstance<>();
-        mttpci.add(entity, ModifyTypeTagPowerType.class, p -> true);
-
-        for (int i = mttpci.getMaxPriority(); i >= mttpci.getMinPriority(); i--) {
-
-            List<ModifyTypeTagPowerType> mttps = mttpci.getPowers(i);
-            if (mttps.isEmpty()) {
-                continue;
-            }
-
-            Optional<Boolean> replaceResult = mttps
-                .stream()
-                .filter(ModifyTypeTagPowerType::shouldReplace)
-                .findFirst()
-                .map(replacingMttp -> replacingMttp.matches(entityTypeTag));
-
-            if (replaceResult.isPresent()) {
-                return replaceResult.get();
-            }
-
-            if (mttps.stream().anyMatch(mttp -> mttp.matches(entityTypeTag))) {
-                return true;
-            }
-
-        }
-
-        return original;
-
+    public static boolean doesApply(Entity entity, RegistryEntryList<EntityType<?>> entryList) {
+        return entryList.getTagKey()
+            .map(tagKey -> doesApply(entity, tagKey))
+            .orElse(false);
     }
 
     public static PowerTypeFactory<?> getFactory() {
         return new PowerTypeFactory<>(
             Apoli.identifier("modify_type_tag"),
             new SerializableData()
-                .add("tag", SerializableDataTypes.ENTITY_TAG)
-                .add("replace", SerializableDataTypes.BOOLEAN, false)
-                .add("priority", SerializableDataTypes.INT, 0),
+                .add("tag", SerializableDataTypes.ENTITY_TAG),
             data -> (powerType, livingEntity) -> new ModifyTypeTagPowerType(powerType, livingEntity,
-                data.get("tag"),
-                data.get("replace"),
-                data.get("priority")
+                data.get("tag")
             )
         ).allowCondition();
     }
