f5dcadcda5f712782b39785b25162107523ea639
eggohito <66972816+eggohito@users.noreply.github.com>
2024-08-12 14:05:35 +0800

    Fixed and tweaked impl. of `modify_player_spawn` power type * Spawn points are now respected, as it used to * The sound event for spawning is now played upon respawning * The search algorithm now uses the entity's world spawn point instead of the overworld dimension's world spawn point + Added `structure_tag` field + Added `biome_tag` field + Added `priority` field

--- src/main/java/io/github/apace100/apoli/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/ServerPlayerEntityMixin.java
@@ -1,6 +1,8 @@
 package io.github.apace100.apoli.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.authlib.GameProfile;
@@ -17,6 +19,7 @@ import io.github.apace100.apoli.power.type.PreventSleepPowerType;
 import io.github.apace100.apoli.util.InventoryUtil;
 import io.github.apace100.apoli.util.PriorityPhase;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.Dismounting;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.StackReference;
 import net.minecraft.item.ItemStack;
@@ -28,6 +31,7 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import net.minecraft.util.Pair;
 import net.minecraft.util.Unit;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -44,7 +48,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Mixin(ServerPlayerEntity.class)
@@ -63,29 +69,32 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity implements Sc
     @Shadow
     public ServerPlayNetworkHandler networkHandler;
 
-    public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
-        super(world, pos, yaw, gameProfile);
-    }
-
     @Shadow
     public abstract void sendMessage(Text message, boolean actionBar);
 
     @Shadow
-    public boolean notInAnyWorld;
+    private boolean spawnForced;
 
-    @Shadow private boolean spawnForced;
+    @Shadow
+    public abstract void setSpawnPoint(RegistryKey<World> dimension, @Nullable BlockPos pos, float angle, boolean forced, boolean sendMessage);
 
-    @Shadow public abstract void setSpawnPoint(RegistryKey<World> dimension, @Nullable BlockPos pos, float angle, boolean forced, boolean sendMessage);
+    @Shadow
+    public abstract void sendMessage(Text message);
 
-    @Shadow public abstract void sendMessage(Text message);
+    @Shadow
+    public abstract boolean shouldDamagePlayer(PlayerEntity player);
 
-    @Shadow public abstract boolean shouldDamagePlayer(PlayerEntity player);
+    @Shadow
+    private float spawnAngle;
 
-    @Shadow public abstract boolean isSpawnForced();
+    @Shadow
+    private static Optional<ServerPlayerEntity.RespawnPos> findRespawnPosition(ServerWorld world, BlockPos pos, float spawnAngle, boolean spawnForced, boolean alive) {
+        throw new AssertionError();
+    }
 
-    @Shadow public abstract float getSpawnAngle();
-
-    @Shadow private float spawnAngle;
+    private ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+        super(world, pos, yaw, gameProfile);
+    }
 
     @Inject(method = "trySleep", at = @At(value = "INVOKE",target = "Lnet/minecraft/server/network/ServerPlayerEntity;setSpawnPoint(Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/util/math/BlockPos;FZZ)V"), cancellable = true)
     public void preventSleep(BlockPos pos, CallbackInfoReturnable<Either<SleepFailureReason, Unit>> info) {
@@ -118,8 +127,8 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity implements Sc
         if (!this.apoli$isEndRespawning() && (this.spawnPointPosition == null || this.apoli$hasObstructedOriginalSpawnPoint())) {
             return PowerHolderComponent.getPowerTypes(this, ModifyPlayerSpawnPowerType.class)
                 .stream()
-                .findFirst()
-                .map(p -> p.dimension)
+                .max(Comparator.comparing(ModifyPlayerSpawnPowerType::getPriority))
+                .map(ModifyPlayerSpawnPowerType::getDimensionKey)
                 .orElse(original);
         }
 
@@ -130,7 +139,7 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity implements Sc
     }
 
     @ModifyReturnValue(method = "getSpawnPointPosition", at = @At("RETURN"))
-    private BlockPos apoli$modifyPlayerSpawnPosition(BlockPos original) {
+    private BlockPos apoli$modifySpawnPointPosition(BlockPos original) {
 
         if (this.apoli$isEndRespawning() || !PowerHolderComponent.hasPowerType(this, ModifyPlayerSpawnPowerType.class)) {
             return original;
@@ -153,11 +162,25 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity implements Sc
 
     @ModifyReturnValue(method = "isSpawnForced", at = @At("RETURN"))
     private boolean apoli$modifySpawnForced(boolean original) {
-        return original
-            || (!this.apoli$isEndRespawning()
-            && (spawnPointPosition == null || this.apoli$hasObstructedOriginalSpawnPoint())
-            && PowerHolderComponent.hasPowerType(this, ModifyPlayerSpawnPowerType.class));
+        return original || (!this.apoli$isEndRespawning() && (spawnPointPosition == null || this.apoli$hasObstructedOriginalSpawnPoint()) && PowerHolderComponent.hasPowerType(this, ModifyPlayerSpawnPowerType.class));
     }
+
+	@WrapOperation(method = "getRespawnTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;findRespawnPosition(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;FZZ)Ljava/util/Optional;"))
+	private Optional<ServerPlayerEntity.RespawnPos> apoli$retryObstructedSpawnPointIfFailed(ServerWorld world, BlockPos pos, float spawnAngle, boolean spawnForced, boolean alive, Operation<Optional<ServerPlayerEntity.RespawnPos>> original) {
+
+	    Optional<ServerPlayerEntity.RespawnPos> originalRespawnPos = original.call(world, pos, spawnAngle, spawnForced, alive);
+
+        if (originalRespawnPos.isEmpty() && PowerHolderComponent.hasPowerType(this, ModifyPlayerSpawnPowerType.class)) {
+            return Optional
+                .ofNullable(Dismounting.findRespawnPos(this.getType(), world, pos, spawnForced))
+                .map(newPos -> ServerPlayerEntity.RespawnPos.fromCurrentPos(newPos, pos));
+        }
+
+        else {
+            return originalRespawnPos;
+        }
+
+	}
 
     @Inject(method = "copyFrom", at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lnet/minecraft/server/network/ServerPlayerEntity;enchantmentTableSeed:I"))
     private void copyInventoryWhenKeeping(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo ci) {
@@ -171,15 +194,16 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity implements Sc
         ServerWorld spawnPointWorld = this.server.getWorld(spawnPointDimension);
         return spawnPointPosition != null
             && spawnPointWorld != null
-            && ServerPlayerEntityAccessor.callFindRespawnPosition(spawnPointWorld, this.spawnPointPosition, this.spawnAngle, this.spawnForced, true).isPresent();
+            && findRespawnPosition(spawnPointWorld, this.spawnPointPosition, this.spawnAngle, this.spawnForced, true).isEmpty();
     }
 
     @Unique
     private BlockPos apoli$findPowerSpawnPoint() {
         return PowerHolderComponent.getPowerTypes(this, ModifyPlayerSpawnPowerType.class)
             .stream()
-            .findFirst()
-            .map(p -> p.getSpawn(true).getRight())
+            .max(Comparator.comparing(ModifyPlayerSpawnPowerType::getPriority))
+            .flatMap(ModifyPlayerSpawnPowerType::getSpawn)
+            .map(Pair::getRight)
             .orElse(null);
     }
 
