6bcfda262653080a06c2f69deab7352bc7bc4f55
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2025-03-18 10:15:06 +1000

    Add modifiers to add velocity actions

--- src/main/java/io/github/apace100/apoli/action/type/bientity/AddVelocityBiEntityActionType.java
+++ src/main/java/io/github/apace100/apoli/action/type/bientity/AddVelocityBiEntityActionType.java
@@ -6,7 +6,10 @@ import io.github.apace100.apoli.action.type.BiEntityActionType;
 import io.github.apace100.apoli.action.type.BiEntityActionTypes;
 import io.github.apace100.apoli.data.ApoliDataTypes;
 import io.github.apace100.apoli.data.TypedDataObjectFactory;
+import io.github.apace100.apoli.util.MiscUtil;
 import io.github.apace100.apoli.util.Space;
+import io.github.apace100.apoli.util.modifier.Modifier;
+import io.github.apace100.apoli.util.modifier.ModifierUtil;
 import io.github.apace100.apoli.util.requirement.BiEntityRequirement;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataType;
@@ -18,6 +21,7 @@ import org.apache.commons.lang3.function.TriConsumer;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3f;
 
+import java.util.List;
 import java.util.function.BiFunction;
 
 public class AddVelocityBiEntityActionType extends BiEntityActionType {
@@ -28,17 +32,29 @@ public class AddVelocityBiEntityActionType extends BiEntityActionType {
             .add("y", SerializableDataTypes.FLOAT, 0F)
             .add("z", SerializableDataTypes.FLOAT, 0F)
             .addFunctionedDefault("velocity", ApoliDataTypes.VECTOR_3_FLOAT, data -> new Vector3f(data.getFloat("x"), data.getFloat("y"), data.getFloat("z")))
+            .add("x_modifier", Modifier.DATA_TYPE, null)
+            .addFunctionedDefault("x_modifiers", Modifier.LIST_TYPE, data -> MiscUtil.singletonListOrEmpty(data.get("x_modifier")))
+            .add("y_modifier", Modifier.DATA_TYPE, null)
+            .addFunctionedDefault("y_modifiers", Modifier.LIST_TYPE, data -> MiscUtil.singletonListOrEmpty(data.get("y_modifier")))
+            .add("z_modifier", Modifier.DATA_TYPE, null)
+            .addFunctionedDefault("z_modifiers", Modifier.LIST_TYPE, data -> MiscUtil.singletonListOrEmpty(data.get("z_modifier")))
             .add("reference", SerializableDataType.enumValue(Reference.class), Reference.POSITION)
             .add("set", SerializableDataTypes.BOOLEAN, false),
         data -> new AddVelocityBiEntityActionType(
             data.get("velocity"),
             data.get("reference"),
-            data.get("set")
+            data.get("set"),
+            data.get("x_modifiers"),
+            data.get("y_modifiers"),
+            data.get("z_modifiers")
         ),
         (actionType, serializableData) -> serializableData.instance()
             .set("velocity", actionType.velocity)
             .set("reference", actionType.reference)
             .set("set", actionType.set)
+            .set("x_modifiers", actionType.xModifiers)
+            .set("y_modifiers", actionType.yModifiers)
+            .set("z_modifiers", actionType.zModifiers)
     );
 
     private final Vector3f velocity;
@@ -46,10 +62,22 @@ public class AddVelocityBiEntityActionType extends BiEntityActionType {
 
     private final boolean set;
 
+    private final List<Modifier> xModifiers;
+    private final List<Modifier> yModifiers;
+    private final List<Modifier> zModifiers;
+
     public AddVelocityBiEntityActionType(Vector3f velocity, Reference reference, boolean set) {
+        this(velocity, reference, set, List.of(), List.of(), List.of());
+    }
+
+
+    public AddVelocityBiEntityActionType(Vector3f velocity, Reference reference, boolean set, List<Modifier> xModifiers, List<Modifier> yModifiers, List<Modifier> zModifiers) {
         this.velocity = velocity;
         this.reference = reference;
         this.set = set;
+        this.xModifiers = xModifiers;
+        this.yModifiers = yModifiers;
+        this.zModifiers = zModifiers;
     }
 
     @Override
@@ -59,6 +87,11 @@ public class AddVelocityBiEntityActionType extends BiEntityActionType {
         Entity target = context.target();
 
         Vector3f velocityCopy = new Vector3f(velocity);
+        velocityCopy.set(
+            ModifierUtil.applyModifiers(actor, this.xModifiers, velocityCopy.x()),
+            ModifierUtil.applyModifiers(actor, this.yModifiers, velocityCopy.y()),
+            ModifierUtil.applyModifiers(actor, this.zModifiers, velocityCopy.z())
+        );
         TriConsumer<Float, Float, Float> method = set
             ? target::setVelocity
             : target::addVelocity;
